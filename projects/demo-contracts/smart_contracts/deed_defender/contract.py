9951cf6fc2cc72d74c6d7196a189422cea37f05b
kraigochieng <kraigochieng@gmail.com>
2024-04-17 16:33:08 +0300

    file updates

--- projects/demo-contracts/smart_contracts/deed_defender/contract.py
+++ projects/demo-contracts/smart_contracts/deed_defender/contract.py
@@ -16,7 +16,7 @@ app = Application(
     descr="This is Deed Defender"
 )
 
-@app.external
+@app.external(authorize=Authorize.only(Global.creator_address()))
 def register_land(land_reference_number: abi.String, title_deed_number: abi.String) -> Expr:
     return Seq(
         app.state.land_reference_number.set(land_reference_number.get()),
@@ -34,14 +34,14 @@ def get_land_details(*, output: abi.String):
     )
 
 @app.external
-def check_land_details(title_deed: abi.String, *, output: abi.Uint64):
-    are_equal = Eq(title_deed.get(), app.state.title_deed_number)
-
-    if are_equal == 1:
-        return output.set(1)
-    else:
-        return output.set(0)
-
+def check_land_details(title_deed: abi.String, *, output: abi.Uint64) -> Expr:
+    return If(
+        title_deed.get() ==app.state.title_deed_number
+    ).Then(
+        output.set(1)
+    ).Else(
+        output.set(0)
+    )
 
 app_spec = app.build()
 # print("======================APP SPEC")
--- projects/demo-contracts/smart_contracts/deed_defender/deploy.py
+++ projects/demo-contracts/smart_contracts/deed_defender/deploy.py
@@ -4,32 +4,67 @@ from pprint import pprint
 contract.app.build().export("../artifacts/deed_defender")
 
 accounts = localnet.kmd.get_accounts()
-sender = accounts[0]
+creator = accounts[0]
+user = accounts[1]
 
-pprint(sender)
 
-app_client = client.ApplicationClient(
+pprint(creator)
+
+creator_app_client = client.ApplicationClient(
     client=localnet.get_algod_client(),
     app=contract.app,
-    sender=sender.address,
-    signer=sender.signer
+    sender=creator.address,
+    signer=creator.signer,
 )
 
-app_id, addr, txid = app_client.create()
+creator_app_id, addr, txid = creator_app_client.create()
+
+user_app_client = client.ApplicationClient(
+    client=localnet.get_algod_client(),
+    app=contract.app,
+    sender=user.address,
+    signer=user.signer,
+    app_id=creator_app_id
+)
+
+user_app_id, addr, txid = user_app_client.create()
 
 print(
     f"""
 Deployed app in txid {txid}
-App ID: {app_id}
+Creator App ID: {creator_app_id}
 App Address: {addr}
 """
 )
 
+print()
 
-app_client.call(contract.register_land, land_reference_number="47/123/456", title_deed_number="ABCD")
-land_details = app_client.call(contract.get_land_details)
-check_1 = app_client.call(contract.check_land_details(title_deed="ABCD"))
-check_2 = app_client.call(contract.check_land_details(title_deed="ABC"))
+print(
+    f"""
+Deployed app in txid {txid}
+User App ID: {user_app_id}
+App Address: {addr}
+"""
+)
+
+creator_app_client.call(contract.register_land, land_reference_number="47/123/456", title_deed_number="ABCD")
+land_details = creator_app_client.call(contract.get_land_details)
+
+check_1 = creator_app_client.call(contract.check_land_details, title_deed="ABCD")
+check_2 = creator_app_client.call(contract.check_land_details, title_deed="ABC")
+
 print(f"register land => {land_details.return_value}")
 print(f"check 1 => {check_1.return_value}")
 print(f"check 2 => {check_2.return_value}")
+
+try:
+    user_app_client.call(contract.register_land, land_reference_number="q", title_deed_number="r")
+    print("changed by another")
+except Exception as e:
+    print("tring to register" + e)
+
+creator_app_client.call(contract.register_land, land_reference_number="b", title_deed_number="e")
+
+print(f"register land => {land_details.return_value}")
+
+
